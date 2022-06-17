d272f094d4ea4d76e38dc6ab54f5a6091acd2248
Jade-KR <kjd8765@gmail.com>
2022-06-18 00:42:17 +0900

    after refactoring 6-8

--- refactoring/ch6/6-8.js
+++ refactoring/ch6/6-8.js
@@ -1,24 +1,38 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+export function readingsOutsideRange(station, range) {
+	return station.readings.filter((r) => !range.contains(r.temp));
+}
+
+class tempRange {
+	#min;
+	#max;
+	constructor(min, max) {
+		this.#min = min;
+		this.#max = max;
+	}
+
+	get min() {
+		return this.#min;
+	}
+
+	get max() {
+		return this.#max;
+	}
+
+	contains(temp) {
+		return temp >= this.#min && temp <= this.#max;
+	}
 }
 
 const station = {
-  name: 'ZB1',
-  readings: [
-    { temp: 47, time: '2016-11-10 09:10' },
-    { temp: 53, time: '2016-11-10 09:20' },
-    { temp: 58, time: '2016-11-10 09:30' },
-    { temp: 53, time: '2016-11-10 09:40' },
-    { temp: 51, time: '2016-11-10 09:50' },
-  ],
+	name: "ZB1",
+	readings: [
+		{ temp: 47, time: "2016-11-10 09:10" },
+		{ temp: 53, time: "2016-11-10 09:20" },
+		{ temp: 58, time: "2016-11-10 09:30" },
+		{ temp: 53, time: "2016-11-10 09:40" },
+		{ temp: 51, time: "2016-11-10 09:50" },
+	],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
+const operationPlan = new tempRange(51, 53);
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+console.log(readingsOutsideRange(station, operationPlan));
