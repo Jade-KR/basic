39578094377c0a78267e635d64aae5288820f4f1
Jade-KR <kjd8765@gmail.com>
2022-06-19 19:32:34 +0900

    after refactoring 9-5

--- refactoring/ch9/9-5.js
+++ refactoring/ch9/9-5.js
@@ -1,20 +1,46 @@
-class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
-  }
+const customerRepository = new CustomerRepository();
 
-  get customer() {
-    return this._customer;
-  }
+const order = new Order(
+	data.number,
+	customerRepository.registerCustomer(data.customerId, "jade")
+);
+class Order {
+	constructor(number, customer) {
+		this._number = data.number;
+		this._customer = customer;
+	}
+
+	get customer() {
+		return this._customer;
+	}
 }
 
 class Customer {
-  constructor(id) {
-    this._id = id;
-  }
+	constructor(id, name) {
+		this._id = id;
+		this._name = name;
+	}
 
-  get id() {
-    return this._id;
-  }
+	get id() {
+		return this._id;
+	}
+}
+
+class CustomerRepository {
+	#customers;
+
+	constructor() {
+		this.#customers = new Map();
+	}
+
+	registerCustomer(id, name) {
+		if (!this.#customers.has(id)) {
+			this.#customers.set(id, new Customer(id, name));
+		}
+		return this.findCustomer(id);
+	}
+
+	findCustomer(id) {
+		return this.#customers.get(id);
+	}
 }
