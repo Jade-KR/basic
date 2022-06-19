e70ea39dd2f9e3e7111a3760c705ee66368fb434
Jade-KR <kjd8765@gmail.com>
2022-06-19 22:47:32 +0900

    after refactoring 11-5

--- refactoring/ch11/11-5.js
+++ refactoring/ch11/11-5.js
@@ -1,23 +1,27 @@
 export class Order {
-  constructor(quantity, itemPrice) {
-    this.quantity = quantity;
-    this.itemPrice = itemPrice;
-  }
+	constructor(quantity, itemPrice) {
+		this.quantity = quantity;
+		this.itemPrice = itemPrice;
+	}
 
-  get finalPrice() {
-    const basePrice = this.quantity * this.itemPrice;
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
-  }
+	get finalPrice() {
+		return this.discountedPrice();
+	}
 
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
-      case 1:
-        return basePrice * 0.95;
-      case 2:
-        return basePrice * 0.9;
-    }
-  }
+	get basePrice() {
+		return this.quantity * this.itemPrice;
+	}
+
+	get discountLevel() {
+		return this.quantity > 100 ? 2 : 1;
+	}
+
+	discountedPrice() {
+		switch (this.discountLevel) {
+			case 1:
+				return this.basePrice * 0.95;
+			case 2:
+				return this.basePrice * 0.9;
+		}
+	}
 }
