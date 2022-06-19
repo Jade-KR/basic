d926dd00914cf9f9aee4b5117cf3dc32fd5a6984
Jade-KR <kjd8765@gmail.com>
2022-06-20 00:24:00 +0900

    after refactoring 12-6

--- refactoring/ch12/12-6.js
+++ refactoring/ch12/12-6.js
@@ -1,26 +1,24 @@
 class Employee {
-  #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
-    this.#name = name;
-    this.#type = type;
-  }
+	#name;
+	constructor(name) {
+		this.#name = name;
+	}
 
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
-  }
+	get type() {
+		return "employee";
+	}
 
-  get type() {
-    return this.#type;
-  }
-
-  toString() {
-    return `${this.#name} (${this.type})`;
-  }
+	toString() {
+		return `${this.#name} (${this.type})`;
+	}
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+class Manager extends Employee {
+	get type() {
+		return "manager";
+	}
+}
+
+const ellie = new Manager("엘리", "engineer");
+const bob = new Employee("밥", "manager");
+console.log(ellie.toString());
