96f7433beacd7fa558dad4aabc76347a76664c18
Jade-KR <kjd8765@gmail.com>
2022-06-19 22:14:32 +0900

    after refactoring 11-1

--- refactoring/ch11/11-1.js
+++ refactoring/ch11/11-1.js
@@ -1,28 +1,30 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
-    (total, each) => each.amount + total,
-    0
-  );
-  sendBill();
-  return result;
+function totalOutstanding() {
+	return customer.invoices.reduce((total, each) => each.amount + total, 0);
+}
+
+function sendBill() {
+	// sendBill ...
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
-  for (const p of people) {
-    if (p === 'Don') {
-      setOffAlarms(alarm, p);
-      return 'Don';
-    }
-    if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
-    }
-  }
-  return '';
+	const miscreants = findMiscreant(people);
+	miscreants.forEach((miscreant) => setOffAlarms(alarm, miscreant));
+}
+
+function findMiscreant(people) {
+	for (const p of people) {
+		if (p === "Don") {
+			return "Don";
+		}
+		if (p === "John") {
+			return "John";
+		}
+	}
+	return "";
 }
 
 function setOffAlarms(alarm, p) {
-  alarm.setOff('Found Miscreant ' + p);
+	alarm.setOff("Found Miscreant " + p);
 }
