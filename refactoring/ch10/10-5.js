deba2ed1fb46aa0fb3ab3fce72ea5ddbd8772aac
Jade-KR <kjd8765@gmail.com>
2022-06-19 21:36:03 +0900

    after refactoring 10-5

--- refactoring/ch10/10-5.js
+++ refactoring/ch10/10-5.js
@@ -1,42 +1,38 @@
-export class Site {
-  constructor(customer) {
-    this._customer = customer;
-  }
+class Hotel {
+	#rooms;
+	constructor() {
+		this.#rooms = [];
+	}
 
-  get customer() {
-    return this._customer;
-  }
+	addRoom(roomNumber) {
+		this.#rooms[roomNumber] = new Room(roomNumber);
+	}
+
+	emptyRoom(roomNumber) {
+		this.#rooms[roomNumber] = new EmptyRoom(roomNumber);
+	}
+
+	cleanRooms() {
+		this.#rooms.forEach((room) => room.clean());
+	}
 }
 
-export class Customer {
-  constructor(name) {
-    this._name = name;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get billingPlan() {
-    //
-  }
-
-  set billingPlan(arg) {
-    //
-  }
-
-  get paymentHistory() {
-    //
-  }
+class Room {
+	constructor(roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	clean() {
+		console.log(`${this.roomNumber} 깨끗하게 청소합니다!`);
+	}
+}
+class EmptyRoom extends Room {
+	clean() {
+		console.log(`${this.roomNumber}번 방이 비어있어요~`);
+	}
 }
 
-// 사용하는 부분
-export function customerName(site) {
-  const aCustomer = site.customer;
-  // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
-
-  return customerName;
-}
+const hotel = new Hotel();
+hotel.addRoom(0);
+hotel.addRoom(1);
+hotel.emptyRoom(1);
+hotel.cleanRooms();
