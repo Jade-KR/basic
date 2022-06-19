812b42cb1b8a0f6d5377d42b62bb93e918a78ff2
Jade-KR <kjd8765@gmail.com>
2022-06-19 22:34:33 +0900

    after refactoring 11-3

--- refactoring/ch11/11-3.js
+++ refactoring/ch11/11-3.js
@@ -1,19 +1,19 @@
-// 예제 1
-function setDimension(name, value) {
-  if (name === 'height') {
-    this._height = value;
-    return;
-  }
-  if (name === 'width') {
-    this._width = value;
-    return;
-  }
+// 예제 1(
+function setWidth(value)) {
+  this._width = value;
 }
 
+function setHeight(value) {
+    this._height = value;
+}
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regularBook(customer) {}
+  premiumBook(customer) {}
+  // 외부에서 사용되지 않는다면
+  #book(customer) {}
 }
 
 // 예제 3
-function setSwitch(on);
+function switchOn() {}
+function switchOff() {}
