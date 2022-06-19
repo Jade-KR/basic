51a84492165310e21c134034b6444c1b79292c4b
Jade-KR <kjd8765@gmail.com>
2022-06-19 20:22:28 +0900

    after refactoring 10-3

--- refactoring/ch10/10-3-1.js
+++ refactoring/ch10/10-3-1.js
@@ -1,22 +1,14 @@
 export function payAmount(employee) {
-  let result;
-  if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
-  }
-  return result;
+	let result;
+	if (employee.isSeparated) {
+		return { amount: 0, reasonCode: "SEP" };
+	}
+
+	return employee.isRetired
+		? { amount: 0, reasonCode: "RET" }
+		: someFinalComputation();
 }
 
 function someFinalComputation() {
-  return { amount: 999, reasonCode: 'UNICORN' };
+	return { amount: 999, reasonCode: "UNICORN" };
 }
